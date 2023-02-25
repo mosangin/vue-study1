abe44d006252f5e91f1e23b4afd6b68b3af553c4
mosangin <75051063+mosangin@users.noreply.github.com>
2023-02-25 17:14:51 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <h2>안녕하세요</h2>
-  <h2>안녕하세요</h2>
+  <div>
+    <h4>{{product[0]}}</h4>
+    <p>{{price1}}</p>
+  </div>
+  <div>
+    <h4>{{product[1]}}</h4>
+    <p>{{priceRandom}}</p>
+  </div>
+  <div>
+    <h4>{{product[2]}}</h4>
+    <p>{{priceRandom}}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      product : ['역삼동원룸','수원원룸','강남원룸'],
+      price1 : 60,
+      priceRandom : '가격은아무거나',
+    }
+  },
   components: {
-    HelloWorld
+   
   }
 }
 </script>
